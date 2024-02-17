91a039743ac24fa4b224a0d1d0bc842949ece0a9
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2024-02-17 23:59:26 +0530

    refactored the code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import "./index.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faUserTie } from "@fortawesome/free-solid-svg-icons";
-
-// import logo from '../logo.png';
-
+import { faUserTie } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
   return (
     <>
       <header>
-        <div >
+        <div>
           <FontAwesomeIcon className="logo" icon={faUserTie} />
         </div>
-        <div>WorkWi$e</div>
+        <div>WorkWi$e - US</div>
       </header>
     </>
   );
