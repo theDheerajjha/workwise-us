c59034d34f1881bbc5a0109ffd3442491ab99871
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2024-03-02 22:10:03 +0530

    Customoixzing footer and importing to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import Header from './components/Header/index';
 import Bannner from './components/Banner/index'
 import List from './components/List/index';
+import Footer from "./components/Footer/index";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   <Header></Header>
   <Bannner></Bannner>
   <List></List>
+  <Footer></Footer>
   </>
   );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import "./index.scss";
 const Footer = () => {
   return (
-    <>Footer!
-    </>
+    <footer className="footer">
+      <div className="container">
+        <p className="text">© 2024 Workwise. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
+
 export default Footer;
