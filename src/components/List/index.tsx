3e49dac1d00fbef2b2e25ed42791bdbd3db17589
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2024-03-10 02:57:04 +0530

    WIP

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthData, hData } from "../../app-config";
 import Pagination from "../Pagination";
+import job_log from "../../Assets/job_icon.png";
 import "./index.scss";
 
 const List = () => {
@@ -16,7 +17,6 @@ const List = () => {
   const handleApply = (jobId: string) => {
     // Navigate to the job description page
     navigate(`/job/${jobId}`);
-    
   };
   const getJobList = () => {
     fetch("https://data.usajobs.gov/api/search", {
@@ -40,16 +40,35 @@ const List = () => {
     <>
       <div className="d-flex main-div flex-wrap justify-content-center">
         {list.map((item: any) => (
-          <div className="card-container" key={item.MatchedObjectId}>
-            <div className="card">
-              <div className="card-header">{item.MatchedObjectDescriptor.OrganizationName || 'unknown'}</div>
-              <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </p>
-                <button className="btn-primary" onClick={() => handleApply(item.MatchedObjectId)}>Apply</button>
+          <div className="job-card-main" key={item.MatchedObjectId}>
+            <div className="job-card-upper">
+              <div className="jobs-logo">
+                <img src={job_log} alt="loading..." />
+              </div>
+              <div className="job-title-exp">
+                <div className="job-designation">Developer</div>
+                <div className="job-company-name">Amazon</div>
+
+                <div className="job-details-short">
+                  <div className="experience-required"> 2+ years</div>
+                  <div className="expected-salary">
+                    As per industry standard
+                  </div>
+                  <div className="job-location">Mumbai</div>
+                </div>
+              </div>
+            </div>
+            <br />
+            <div className="job-card-lower">
+              <div className="apply-before-text">Apply Before :</div>
+              <div className="apply-button-div">
+                {" "}
+                <button
+                  className="btn-primary apply-button"
+                  onClick={() => handleApply(item.MatchedObjectId)}
+                >
+                  Apply
+                </button>
               </div>
             </div>
           </div>
@@ -60,4 +79,15 @@ const List = () => {
   );
 };
 
+/* <div className="card">
+              <div className="card-header">{item.MatchedObjectDescriptor.OrganizationName || 'unknown'}</div>
+              <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <button className="btn-primary" onClick={() => handleApply(item.MatchedObjectId)}>Apply</button>
+              </div>
+   </div> */
 export default List;
