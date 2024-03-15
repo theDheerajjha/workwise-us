fcfbd13b969e910ece0a024546e4be6ef28034d9
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2024-03-15 23:58:05 +0530

    replaced the hardcoded with actual data

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,41 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { AuthData, hData } from "../../app-config";
+import { hData } from "../../app-config";
 import Pagination from "../Pagination";
 import job_log from "../../Assets/job_icon.png";
 import "./index.scss";
 
 const List = () => {
-  const [list, setList] = useState<any>([]);
+  const [list, setList] = useState<any[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     setList(hData);
-    // getJobList();
   }, []);
 
   const handleApply = (jobId: string) => {
     // Navigate to the job description page
     navigate(`/job/${jobId}`);
   };
-  const getJobList = () => {
-    fetch("https://data.usajobs.gov/api/search", {
-      method: "GET",
-      headers: {
-        Host: AuthData.hostAddress,
-        "User-Agent": AuthData.userAgent,
-        "Authorization-Key": AuthData.authKey,
-      },
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        setList(result);
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+
   return (
     <>
       <div className="d-flex main-div flex-wrap justify-content-center">
@@ -46,15 +28,15 @@ const List = () => {
                 <img src={job_log} alt="loading..." />
               </div>
               <div className="job-title-exp">
-                <div className="job-designation">Developer</div>
-                <div className="job-company-name">Amazon</div>
+                <div className="job-designation">{item.MatchedObjectDescriptor.PositionTitle}</div>
+                <div className="job-company-name">{item.MatchedObjectDescriptor.OrganizationName}</div>
 
                 <div className="job-details-short">
-                  <div className="experience-required"> 2+ years</div>
+                  <div className="experience-required"> {item.MatchedObjectDescriptor.UserArea.Details.RequiredExperience}</div>
                   <div className="expected-salary">
-                    As per industry standard
+                    {`Salary Range: $${item.MatchedObjectDescriptor.PositionRemuneration[0].MinimumRange} - $${item.MatchedObjectDescriptor.PositionRemuneration[0].MaximumRange} per ${item.MatchedObjectDescriptor.PositionRemuneration[0].Description}`}
                   </div>
-                  <div className="job-location">Mumbai</div>
+                  <div className="job-location">{item.MatchedObjectDescriptor.PositionLocationDisplay}</div>
                 </div>
               </div>
             </div>
