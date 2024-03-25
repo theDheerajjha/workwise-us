c5d7aa11a4af4f8a45ff80d4f309b786730db760
Dheeraj Kumar Jha <52864945+theDheerajjha@users.noreply.github.com>
2024-03-25 23:58:23 +0530

    removed unused import link

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 import { hData } from "../../temp-data";
 import job_log from "../../Assets/job_icon.png";
 import "./index.scss";
@@ -7,7 +7,6 @@ import "./index.scss";
 const List = () => {
   const [list, setList] = useState<any[]>([]);
   const navigate = useNavigate();
-
   useEffect(() => {
     setList(hData);
   }, []);
